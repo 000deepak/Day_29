b9b45f4230502831ca4acd01f14cd7e8e7e6372d
unknown <kumbhardeepak000@gmail.com>
2021-11-14 20:27:17 +0530

    added:method to load data

---
+++ src/main/java/cencus_analyzer/CensusAnalyzerException.java
@@ -0,0 +1,21 @@
+package cencus_analyzer;
+
+class CensusAnalyserException extends Exception {
+
+    public enum ExceptionType {
+        CENSUS_FILE_INCORRECT, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER;
+    }
+
+    public ExceptionType type;
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "CensusAnalyserException{" +
+                "type=" + type +
+                '}';
+    }
+}
